47936371f63c2a8e1aa52484a9e7d392273eedc5
Lysandre <lysandre.debut@reseau.eseo.fr>
2024-03-08 10:58:13 +0100

    Adds pretrained IDs directly in the tests

--- tests/models/biogpt/test_tokenization_biogpt.py
+++ tests/models/biogpt/test_tokenization_biogpt.py
@@ -25,6 +25,7 @@ from ...test_tokenization_common import TokenizerTesterMixin
 
 
 class BioGptTokenizationTest(TokenizerTesterMixin, unittest.TestCase):
+    from_pretrained_id = "microsoft/biogpt"
     tokenizer_class = BioGptTokenizer
     test_rust_tokenizer = False
 
